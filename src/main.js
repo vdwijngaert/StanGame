29b03f4bd9dac8cb8f1838a63e2274355d73e762
Koen Van den Wijngaert <koen@neok.be>
2026-05-23 11:41:47 +0200

    feat: wire Phaser game with all scenes

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Phaser from 'phaser';
+import { BootScene } from './scenes/BootScene.js';
+import { BirthdayScene } from './scenes/BirthdayScene.js';
+import { GameScene } from './scenes/GameScene.js';
+import { GameOverScene } from './scenes/GameOverScene.js';
+
+new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 480,
+  height: 854,
+  backgroundColor: '#111111',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [BootScene, BirthdayScene, GameScene, GameOverScene],
+});
